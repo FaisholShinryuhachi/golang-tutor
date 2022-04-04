cd2628fec831f2aa5c8062e63b79eaa83613f806
FaisholShinryuhachi <fahmifaishol11@gmail.com>
2022-04-04 23:23:23 +0700

    kedua

---
+++ kedua/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string = "Golang"
+
+	// Jika ingin langsung asign varible tanpa type data
+	age := 20
+
+	fmt.Println(name)
+	fmt.Println(age)
+}
